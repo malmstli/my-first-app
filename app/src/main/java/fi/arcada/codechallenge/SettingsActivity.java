f5d91168216fb8464c8990288b5f54627981194f
malmstli <malmstli@arcada.fi>
2025-04-22 17:48:42 +0300

    CC lektion 6

--- app/src/main/java/fi/arcada/codechallenge/SettingsActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/SettingsActivity.java
@@ -1,32 +1,40 @@
 package fi.arcada.codechallenge;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.SharedPreferences;
-
 public class SettingsActivity extends AppCompatActivity {
-    private Button resetButton;
     private SharedPreferences sharedPreferences;
+    private Button resetButton;
+    private Button saveButton;
+    private EditText inputText;
 
-    // Återanvänd koden i MainActivity, så vi använder samma sharedPreference fil och "räknare"
     private static final String PREFS_NAME = "AppCounterPrefs";
     private static final String APP_COUNTER_KEY = "appCounter";
+    private static final String SAVED_TEXT_KEY = "savedText";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
-        // Helt vanlig knapp som vi hittar
-        // På knappen sätter vi funktionen att nolla vår räknare
-        // - Samma sätt som vi uppdatera den, kan vi sätta den till noll
-        // --- Man kan göra om denna knapp till Spara knappen!
-
+        inputText = findViewById(R.id.inputText);
+        saveButton = findViewById(R.id.saveButton);
         resetButton = findViewById(R.id.resetButton);
+
         sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+
+        saveButton.setOnClickListener(v -> {
+            String text = inputText.getText().toString();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(SAVED_TEXT_KEY, text);
+            editor.apply();
+            finish();
+        });
 
         resetButton.setOnClickListener(v -> {
             SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -34,4 +42,5 @@ public class SettingsActivity extends AppCompatActivity {
             editor.apply();
         });
     }
+
 }
